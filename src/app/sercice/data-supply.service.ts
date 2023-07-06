99a4b20eb4bd42498c50c67d1e1eb2a31224b952
Sandesh-Pol <sandeshpol268@gmail.com>
2023-07-06 11:49:02 +0530

    id

--- src/app/sercice/data-supply.service.ts
+++ src/app/sercice/data-supply.service.ts
@@ -5,6 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class DataSupplyService {
  
+
+    // 1 - Darama
+    // 2 - Action
+    // 3 - Romantic
+    // 4 - Horror
+    // 5 - Romantic
+
+
+
   constructor() { }
   
   sendData(data: any) {
@@ -13,27 +22,27 @@ export class DataSupplyService {
 
   currentMovie = [
 
-    { src: 'assets/img1.jpg', movieName: 'Satyaprem Ki Katha', type: 'HINDI | (U/A) | DRAMA' },
+    { src: 'assets/img1.jpg', movieName: 'Satyaprem Ki Katha', type: 'HINDI | (U/A) | DRAMA', id:1 },
 
 
-    { src: 'assets/img2.jpg', movieName: 'Carry on Jatta 3', type: 'PUNJABI | (U/A) | DRAMA' },
+    { src: 'assets/img2.jpg', movieName: 'Carry on Jatta 3', type: 'PUNJABI | (U/A) | DRAMA', id:1 },
 
-    { src: 'assets/img3.jpg', movieName: 'INDIANA JONES PART 4', type: 'ENGLISH | HINDI | (U/A) | ACTION' },
+    { src: 'assets/img3.jpg', movieName: 'INDIANA JONES PART 4', type: 'ENGLISH | HINDI | (U/A) | ACTION', id:2 },
 
 
-    { src: 'assets/img4.jpg', movieName: 'ADIPURUSH ', type: 'HINDI | (U) | ACTION' },
+    { src: 'assets/img4.jpg', movieName: 'ADIPURUSH ', type: 'HINDI | (U) | ACTION', id:2 },
 
     {
       src: 'assets/img5.jpg', movieName: 'ZARA HATKE ZARA BACHKE', type: 'HINDI | (U/A) | ROMANCE'
-    },
+   , id:3 },
 
-    { src: 'assets/img6.jpg', movieName: 'THE FLASH', type: 'ENGLISH | HINDI | (U/A) | ACTION' },
+    { src: 'assets/img6.jpg', movieName: 'THE FLASH', type: 'ENGLISH | HINDI | (U/A) | ACTION', id:2 },
 
     {
       src: 'assets/img7.jpg', movieName: '1920 HORRORS', type: 'HINDI | (A) | HORROR'
-    },
+   , id:4 },
 
-    { src: 'assets/img8.jpg', movieName: 'ELEMENTAL', type: 'ENGLISH | HINDI | (U/A) | ANIMATION' },
+    { src: 'assets/img8.jpg', movieName: 'ELEMENTAL', type: 'ENGLISH | HINDI | (U/A) | ANIMATION', id:5 },
   ]
 
   sendData1(data: any) {
