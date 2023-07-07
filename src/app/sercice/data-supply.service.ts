475c7e3362f84d4f8133a6b1ac83c01fa27bef85
Sandesh-Pol <sandeshpol268@gmail.com>
2023-07-07 20:13:04 +0530

    clre

--- src/app/sercice/data-supply.service.ts
+++ src/app/sercice/data-supply.service.ts
@@ -11,32 +11,32 @@ export class DataSupplyService {
 
   currentMovie = [
 
-    { src: 'assets/img1.jpg', movieName: 'Satyaprem Ki Katha', type: 'HINDI | (U/A) | DRAMA', id:'drama-4'},
+    { src: 'assets/img1.jpg', movieName: 'Satyaprem Ki Katha', type: 'HINDI | (U/A) | DRAMA', id:'drama-4', idAll: 'all-0' },
 
 
-    { src: 'assets/img2.jpg', movieName: 'Carry on Jatta 3', type: 'PUNJABI | (U/A) | DRAMA', id:'drama-4' },
+    { src: 'assets/img2.jpg', movieName: 'Carry on Jatta 3', type: 'PUNJABI | (U/A) | DRAMA', id:'drama-4', idAll: 'all-0' },
 
-    { src: 'assets/img3.jpg', movieName: 'INDIANA JONES PART 4', type: 'ENGLISH | HINDI | (U/A) | ACTION', id:'action-1' },
+    { src: 'assets/img3.jpg', movieName: 'INDIANA JONES PART 4', type: 'ENGLISH | HINDI | (U/A) | ACTION', id:'action-1', idAll: 'all-0' },
 
 
-    { src: 'assets/img4.jpg', movieName: 'ADIPURUSH ', type: 'HINDI | (U) | ACTION', id:'action-1' },
+    { src: 'assets/img4.jpg', movieName: 'ADIPURUSH ', type: 'HINDI | (U) | ACTION', id:'action-1', idAll: 'all-0' },
 
     {
       src: 'assets/img5.jpg', movieName: 'ZARA HATKE ZARA BACHKE', type: 'HINDI | (U/A) | ROMANCE'
-   , id:'romance-6' },
+   , id:'romance-6', idAll: 'all-0' },
 
-    { src: 'assets/img6.jpg', movieName: 'THE FLASH', type: 'ENGLISH | HINDI | (U/A) | ACTION', id:'action-1' },
+    { src: 'assets/img6.jpg', movieName: 'THE FLASH', type: 'ENGLISH | HINDI | (U/A) | ACTION', id:'action-1', idAll: 'all-0' },
 
     {
       src: 'assets/img7.jpg', movieName: '1920 HORRORS', type: 'HINDI | (A) | HORROR'
-   , id:'horror-5' },
+   , id:'horror-5', idAll: 'all-0' },
 
-    { src: 'assets/img8.jpg', movieName: 'ELEMENTAL', type: 'ENGLISH | HINDI | (U/A) | ANIMATION', id:'animation-2' },
+    { src: 'assets/img8.jpg', movieName: 'ELEMENTAL', type: 'ENGLISH | HINDI | (U/A) | ANIMATION', id:'animation-2', idAll: 'all-0' },
   ]
 
   sendData1(data: any) {
     this.currentMovie = data;
-  }
+   }
 
   upcomingMovie = [
 
@@ -64,7 +64,7 @@ export class DataSupplyService {
   ]
 
     sendMovieData() {
-    this.currentMovie1 = this.currentMovie.filter(movie => movie.id === 'all-0');
+    this.currentMovie1 = this.currentMovie.filter(movie => movie.idAll === 'all-0');
    return this.currentMovie1;
  }
  sendMovieData1() {
@@ -72,6 +72,7 @@ export class DataSupplyService {
  }
 
  genarateDinamicData(id:any){
+  
   this.currentMovie1 = this.currentMovie.filter(movie => movie.id === id);
 }
 }
